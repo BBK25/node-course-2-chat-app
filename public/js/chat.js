a1ee831c51d34d519c5b4ec56df265106daac2a5
Benjamin <balogh.benjamin1991@gmail.com>
2017-10-13 15:38:15 +0200

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,7 +16,17 @@ function scrollToBottom() {
 }
 
 socket.on('connect', function() {
-  console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err);
+      window.location.href= '/';
+    } else {
+      console.log('No error');
+    }
+  });
+  // console.log('Connected to server');
 
   // socket.emit('createMessage', {
   //   from: 'Andrew',
@@ -31,6 +41,20 @@ socket.on('connect', function() {
 
 socket.on('disconnect', function() {
   console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function (users) {
+  // console.log('Users list', users);
+  var ol = jQuery('<ol></ol>');
+  var lista = "";
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+    // ol.append('<li>'+user+'</li>');
+    // console.log(user);
+  });
+
+  jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function(message) {
